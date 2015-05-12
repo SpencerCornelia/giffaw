b3dfd32fb8161ba0359c738166ea9830666a92d2
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-05-11 19:45:43 -0700

    finished both problems + 2nd bonus

--- base.js
+++ base.js
@@ -1,4 +1,25 @@
-$(document).ready(function(){
-  // code in here!
-  console.log("here's your data:", mockData)
-})
+$(function() {
+
+	$("#inputBox").on("change", function(event) {
+		var userInput = ($(this).val());
+		event.preventDefault();
+		var userSearch = ("http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=dc6zaTOxFJmzC");
+		$.get(userSearch, render);
+	})
+	var trendingPage = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=25"//&limit=25 <-- limits results to 25 at end
+	$.get(trendingPage, newRender);
+});
+
+var render = function(data) {
+	var newImages = data.data;
+	newImages.forEach(function(item,i) {
+		$("body").append("<img src='" + newImages[i].images.fixed_height.url + "'>");
+	})
+}
+
+var newRender = function(data) {
+	var newImages = data.data;
+	newImages.forEach(function(item,i) {
+		$("body").append("<img src='" + newImages[i].images.fixed_height.url + "'>");
+	})
+}
